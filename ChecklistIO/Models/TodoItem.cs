b40eefb141ec891b1e7c35ce2373b8bd2aae5807
acarroll94 <alexandertcarroll@gmail.com>
2017-09-16 00:51:19 -0400

    Initial commit.

---
+++ ChecklistIO/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChecklistIO.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public int Priority { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime FinishBy { get; set; }
+    }
+}
