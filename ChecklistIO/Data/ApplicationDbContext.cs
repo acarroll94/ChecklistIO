dd264e9e66fadd08fd67cd0aea4e9cc5ca330dc5
acarroll94 <alexandertcarroll@gmail.com>
2017-09-21 16:57:30 -0400

    Added new controller

--- ChecklistIO/Data/ApplicationDbContext.cs
+++ ChecklistIO/Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace ChecklistIO.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<ChecklistIO.Models.TodoItem> TodoItem { get; set; }
     }
 }
--- ChecklistIO/Data/Migrations/20170921204336_InitialDbMigration0.Designer.cs
+++ ChecklistIO/Data/Migrations/20170921204336_InitialDbMigration0.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ChecklistIO.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170921204336_InitialDbMigration0")]
+    partial class InitialDbMigration0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
